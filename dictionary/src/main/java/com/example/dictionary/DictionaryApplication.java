c6b7a97c7606aba59d17d5f28d45d1d73816838e
ntbolton <ntbolton03@gmail.com>
2023-11-21 19:31:01 -0700

    added dictionary reference code

--- dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
+++ dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -2,12 +2,21 @@ package com.example.dictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 @SpringBootApplication
 public class DictionaryApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(DictionaryApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(DictionaryApplication.class, args);
+
+        log.info("hello spring");
     }
+
 
 }
