949d57c5790f2e02357b66808eaad25e7588fa36
ntbolton <ntbolton03@gmail.com>
2023-12-14 13:45:25 -0700

    project 6 dictionary additions

--- dictionary/src/main/java/com/example/dictionary/controller/DictionaryController.java
+++ dictionary/src/main/java/com/example/dictionary/controller/DictionaryController.java
@@ -65,6 +65,27 @@ public class DictionaryController {
         return entries;
     }
 
+    @GetMapping("/getWordsEndingWith/{value}")
+    public List<Entry> getWordsEndingWith(@PathVariable String value) throws WordNotFoundException {
+
+        StopWatch sw = new StopWatch();
+        sw.start();
+        List<Entry> entries = this.dictionaryService.getWordsEndingWith(value);
+        sw.stop();
+
+        long nanoSeconds = sw.getLastTaskTimeNanos();
+        String message = new StringBuilder("Retrieved entries for words ending with [")
+                .append(value)
+                .append("] containing ")
+                .append(entries.size())
+                .append(" entries in ")
+                .append(nanoSeconds / 1000000.0)
+                .append("ms")
+                .toString();
+        logger.info(message);
+        return entries;
+    }
+
     @GetMapping("/getWordsThatContain/{value}")
     public List<Entry> getWordsThatContain(@PathVariable String value) throws WordNotFoundException {
 
--- dictionary/src/main/java/com/example/dictionary/service/DictionaryService.java
+++ dictionary/src/main/java/com/example/dictionary/service/DictionaryService.java
@@ -73,6 +73,17 @@ public class DictionaryService {
                 .collect(Collectors.toList());
 
     }
+    public List<Entry> getWordsEndingWith(String value) {
+
+        return DictionaryReference.getDictionary()
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().endsWith(value))
+                .sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                .map(entry -> new Entry(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+
+    }
 
 
 }
